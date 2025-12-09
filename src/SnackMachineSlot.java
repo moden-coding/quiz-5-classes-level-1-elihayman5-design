76a2f940aee3e14f5f457b2ea23e62bea8608fa4
Eli <29ehayman@gannacademy.org>
2025-12-09 10:16:04 -0500

    done

--- src/SnackMachineSlot.java
+++ src/SnackMachineSlot.java
@@ -1,3 +1,43 @@
 public class SnackMachineSlot {
+    private String itemName;
+    private int quantity;
+    private double price;
+    private int itemsSold;
+
+
+public SnackMachineSlot(String givenItemName, double initialPrice, int initialQuantity, int itemsSold) {
+   
+   itemName = "Chips";
+   
+   quantity = 3;
+   if (quantity < 0)
+    quantity = 0;
+   
+   price = 1.5;
+     if (price < 0)
+        price = 0;
+
+     itemsSold = 0;
     
 }
+
+
+public String toString() {
+return ( "Item: " + itemName + "," + " Price: " +  price + "," + " In Stock: " + quantity);
+}
+
+public void purchaseOne() {
+    if (quantity > 0)
+        quantity = quantity - 1;
+    itemsSold = itemsSold + 1;
+    
+      
+}
+
+public double getRevenue() {
+    return itemsSold * price; 
+}
+
+}
+
+
